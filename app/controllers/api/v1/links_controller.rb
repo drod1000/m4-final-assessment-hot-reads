df5006d9a1fb26cfb6cd643515a2d415b15d9702
Daniel Rodriguez <drod2793@gmail.com>
2017-04-17 12:49:19 -0600

    API endpoint creates link if it doesn't exist yet

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::LinksController < ApplicationController
   def create
+    @link = Link.find_by(url: params[:url])
+    if(!@link)
+      Link.create(link_params)
+      render :nothing => true, :status => 201
+    end
   end
 
   def link_params
