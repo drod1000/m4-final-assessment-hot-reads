7875041440bc694ba7bd71bf38646499851ee7a2
Daniel Rodriguez <drod2793@gmail.com>
2017-04-17 12:52:42 -0600

    Adds test for post request for link that already exists

--- spec/requests/api/v1/links_spec.rb
+++ spec/requests/api/v1/links_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
-describe "Links API" do
-  it "can create a link" do
+describe 'Links API' do
+  it 'can create a link' do
     post '/api/v1/links', :url => 'http://someurl.com'
 
     expect(response).to be_success
     expect(response.status).to eq(201)
     expect(Link.count).to eq(1)
+  end
+
+  it 'can increment count if link already exists' do
+    link = create(:link, url: 'http://someurl.com', read: 5)
+
+    post '/api/v1/links', :url => 'http://someurl.com'
+
+    expect(response).to be_success
+    expect(response.status).to eq(202)
+    expect(link.read).to eq(6)
   end
 end
