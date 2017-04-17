ee3f0177fc7c7c269c1322a38274dfaa5c808893
Daniel Rodriguez <drod2793@gmail.com>
2017-04-17 12:45:01 -0600

    Adds link params to links controller

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::LinksController < ApplicationController
   def create
   end
+
+  def link_params
+    params.permit(:url)
+  end
 end
--- spec/requests/api/v1/links_spec.rb
+++ spec/requests/api/v1/links_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "Links API" do
   it "can create a link" do
-    post '/api/v1/links', :link => {:url => 'http://someurl.com'}
+    post '/api/v1/links', :url => 'http://someurl.com'
 
     expect(response).to be_success
     expect(response.status).to eq(201)
